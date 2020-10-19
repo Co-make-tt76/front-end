aebac1059f7f1313525164293179de59d7c1e21a
tarmes <traarmes@gmail.com>
2020-10-19 17:56:12 -0400

    basic issueDisplay and issue component

---
+++ co-make/src/components/Issue.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+   Card, CardText, CardBody,
+   CardTitle, CardSubtitle, Button
+ } from 'reactstrap';
+
+export default function Issue(props) {
+
+   const { issue, upvote } = props;
+
+   return (
+      <div className='issue-container'>
+         <Card>
+            <CardBody>
+               <CardText>Report: {issue.report}</CardText>
+               <CardText>Proposed Solution: {issue.proposedSolution}</CardText>
+               <div className='big-location-container'>
+                  <CardTitle className='location'>Location:</CardTitle>
+                  <div className='location-container'>
+                     <p>{issue.incidentLocation}</p>
+                     <p>{issue.city}, {issue.state} {issue.zip}</p>
+                  </div>
+               </div>
+               <Button className='edit-button'>Edit</Button>
+               <div className='upvote-container'>
+                  <Button className='upvote-button' onClick={upvote}>Upvote</Button>
+                  <p>{issue.upvoteCount}</p>
+               </div>
+            </CardBody>
+         </Card>
+      </div>
+   )
+}
