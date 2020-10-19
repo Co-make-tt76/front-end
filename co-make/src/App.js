114f6b461e8ab3e56332d8c659a6c0a0c02c4522
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-19 13:23:00 -0700

    tabbed sign up and login forms with validation'

--- co-make/src/App.js
+++ co-make/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import IssuesList from './components/IssuesList';
+import LoginRegisterTab from './components/LoginRegisterTab'
 import Login from './components/Login';
 import Register from './components/Register';
 import AddNewIssue from './components/AddNewIssue';
@@ -12,12 +13,14 @@ function App() {
 		<div className="App">
 			<NavBar />
 			<p>HELLO CO-MAKE TEAM</p>
+			
+			
 
 			<div className='routes'>
 				<Switch>
 					<Route exact path={["/", "/issues"]} component={IssuesList} />
-					<Route path="/login" component={Login} />
-					<Route path='/register' component={Register} />
+					<Route path="/login" component={() => <LoginRegisterTab tab={'2'} />} />
+					<Route path='/register' component={() => <LoginRegisterTab tab={'1'} /> } />
 					<Route path='/addIssue' component={AddNewIssue} />
 				</Switch>
 			</div>
--- co-make/src/components/Login.js
+++ co-make/src/components/Login.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useRef }from 'react';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import {Form, Button, Label, Input, FormGroup, Col, Row} from 'reactstrap'
 
 export default function Login() {
+   const { register, handleSubmit, errors, reset } = useForm({ 
+      mode: "onBlur", 
+    });
+  const onSubmit = (data) => { 
+    
+    console.log(data)
+    reset()
+  }
+  console.log("MY ERRORS FROM REG FORM=>", errors)
+    
+
+
    return (
-      null
+   <div className='auth-form-container  shadow' >
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input 
+        type="email" 
+        placeholder="email" 
+        name="email"
+        invalid={errors.email ? true : false}
+        innerRef={register({required: "Email is required"})} 
+      
+        />
+        <ErrorMessage errors={errors} name="email" />
+      </FormGroup>
+      
+        <FormGroup>
+          <Label for="password" >Password</Label>
+          <Input 
+          type="password" 
+          placeholder="Password" 
+          name="password" 
+          invalid={errors.password ? true : false}
+          innerRef={register({required: "Password is required.",  maxLength: 100}, )} 
+          />
+          <ErrorMessage errors={errors} name="password" />
+      </FormGroup>
+
+      <Button type="submit" color="primary">Sign Up</Button>
+      
+    </Form>
+    </div>
+    
    )
 }
