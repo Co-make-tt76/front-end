44bb4fb648fd022770253abf17375c6e407c3a06
tarmes <traarmes@gmail.com>
2020-10-20 15:21:09 -0400

    redux store created and provided

--- co-make/src/index.js
+++ co-make/src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { reducer } from './store/reducers/index';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(  
-  <Router>
-    <App />
-  </Router>,
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk, logger)
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>  ,
   document.getElementById('root')
 );
 
