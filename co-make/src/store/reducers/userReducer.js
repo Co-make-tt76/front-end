c244dacb36ef07214c0d150b3d2dea52ed5197f5
tarmes <traarmes@gmail.com>
2020-10-22 10:58:44 -0400

    made a piece of state for loggedIN

--- co-make/src/store/reducers/userReducer.js
+++ co-make/src/store/reducers/userReducer.js
@@ -1,5 +1,16 @@
-export const userReducer = (state = null, action) => {
+import { LOG_USER_IN } from "../variables";
+
+const initialState = {
+   isLoggedIn: false
+}
+
+export const userReducer = (state = initialState, action) => {
    switch (action.type) {
+      case LOG_USER_IN:
+         return {
+            ...state,
+            isLoggedIn: true
+         }
       default:
          return state;
    }
