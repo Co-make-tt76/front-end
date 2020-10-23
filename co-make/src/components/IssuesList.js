7975ce5ac612b3f110288f448719d76daac0c01d
tarmes <traarmes@gmail.com>
2020-10-22 20:09:55 -0400

    added private route

--- co-make/src/components/IssuesList.js
+++ co-make/src/components/IssuesList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Issue from './Issue';
 import { connect } from 'react-redux';
 import { fetchIssues } from '../store/actions/issuesActions';
